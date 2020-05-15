e2a444824a2e4b45fa94672941a13e0440d329bf
umha <27706842642@qq.com>
2020-05-15 20:59:34 +0800

    增加角色模块仓库

---
+++ Blog.Core/Repositories/Module/IRoleModuleRepository.cs
@@ -0,0 +1,8 @@
+﻿using Blog.Core.Repositories.Module.Inputs;
+
+namespace Blog.Core.Repositories.Module
+{
+    public interface IRoleModuleRepository : IBaseRepository<RoleModuleInput>
+    {
+    }
+}
---
+++ Blog.Core/Repositories/Module/Inputs/RoleModuleInput.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Blog.Core.Repositories.Module.Inputs
+{
+    public class RoleModuleInput : Entity
+    {
+        public Guid UserId { get; set; }
+
+        public Guid ModuleId { get; set; }
+    }
+}
